8725897afc8e6a37cd17c0269479e943d36a74fd
JonasCGN <jonasbo66@gmail.com>
2024-05-11 15:34:52 -0300

    att

--- main.c
+++ main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <unistd.h>
 
 typedef struct time
 {
+  int qtdJogosJogados;
   char *nome;
   int pontuacao;
-  int *qtdPartidas;
+  int *timesJogados;
   pthread_mutex_t mutex_pont;
   pthread_t threadTime;
 } Time;
@@ -14,43 +16,33 @@ typedef struct time
 #define QTD_TIME 20
 
 Time *times;
-/*
-Thread Time
-*/
-void *threadTime(void *arg){
+int jogosCoordenados[QTD_TIME][QTD_TIME] = {0};
+
+void *threadTime(void *arg)
+{
   int *idx = (int *)arg;
 
-  for (int x = 0; x < QTD_TIME; x++)
-  {
-    if (*idx == x)
-      continue;
-    
-    if (times[*idx].qtdPartidas[x] > 0)
-      continue;
-
-    pthread_mutex_lock(&times[*idx].mutex_pont);
-
-    times[*idx].qtdPartidas[x]++;
-    times[*idx].pontuacao += 1;
-
-    pthread_mutex_unlock(&times[*idx].mutex_pont);
-  }
+  pthread_mutex_lock(&times[*idx].mutex_pont);
 
   for (int x = 0; x < QTD_TIME; x++)
   {
-    if (*idx == x)
+    if (*idx == x || jogosCoordenados[*idx][x] || jogosCoordenados[x][*idx])
       continue;
 
-    if (times[*idx].qtdPartidas[x] > 0)
-      continue;
+    jogosCoordenados[*idx][x] = 1;
+    jogosCoordenados[x][*idx] = 1;
 
-    pthread_mutex_lock(&times[*idx].mutex_pont);
+    times[*idx].qtdJogosJogados += 1;
+    times[x].qtdJogosJogados += 1;
 
-    times[*idx].qtdPartidas[x]++;
+    times[*idx].timesJogados[x] = 1;
     times[*idx].pontuacao += 1;
 
-    pthread_mutex_unlock(&times[*idx].mutex_pont);
+    printf("Time %02d ganhou de %02d\n", *idx, x);
+    // usleep(100000); // Adiciona um pequeno atraso para simular o jogo
   }
+
+  pthread_mutex_unlock(&times[*idx].mutex_pont);
 
   free(idx);
   return NULL;
@@ -65,8 +57,10 @@ int main()
     int *aux = (int *)malloc(sizeof(int));
     *aux = x;
 
+    times[x].qtdJogosJogados = 0;
     times[x].pontuacao = 0;
-    times[x].qtdPartidas = (int *)calloc(sizeof(int), QTD_TIME);
+    times[x].timesJogados = (int *)calloc(QTD_TIME, sizeof(int));
+    pthread_mutex_init(&times[x].mutex_pont, NULL);
 
     pthread_create(&times[x].threadTime, NULL, &threadTime, (void *)aux);
   }
@@ -74,18 +68,14 @@ int main()
   for (int x = 0; x < QTD_TIME; x++)
     pthread_join(times[x].threadTime, NULL);
 
-  // for (int x = 0; x < QTD_TIME; x++)
-  // {
-  //   for (int y = 0; y < QTD_TIME; y++)
-  //     printf("[%d]", times[x].qtdPartidas[y]);
-  //   printf("\n");
-  // }
-
-  for(int x = 0 ;  x < QTD_TIME ; x++)
-    printf("Time %d: %d\n", x, times[x].pontuacao);
-
   for (int x = 0; x < QTD_TIME; x++)
-    free(times[x].qtdPartidas);
+  {
+    printf("Time %d: %d\n", x, times[x].pontuacao);
+    printf("Time %d jogou %d jogos\n", x, times[x].qtdJogosJogados);
+    pthread_mutex_destroy(&times[x].mutex_pont);
+    free(times[x].timesJogados);
+  }
+
   free(times);
   return 0;
 }
